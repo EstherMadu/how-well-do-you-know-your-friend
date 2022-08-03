984230225a6779b8effe4f046a5a080048a62ae3
esthermadu <prityexsta@gmail.com>
2022-08-03 22:51:27 +0100

    dynamic table

--- assets/scripts/result.js
+++ assets/scripts/result.js
@@ -1,12 +1,21 @@
 const playersName = localStorage.getItem("players-name");
 
+const table = document.getElementById("table");
+console.log(table);
+
 const playerName = document.getElementById("input-name");
 
-playerName.textContent = localStorage.getItem("players-name");
+// playerName.textContent = localStorage.getItem("players-name");
 
 const playerScore = document.getElementById("score");
-playerScore.textContent = localStorage.getItem("result-score");
+// playerScore.textContent = localStorage.getItem("result-score");
 
 const playerResult = localStorage.getItem("object");
 const resultLoad = JSON.parse(playerResult);
 console.log(resultLoad);
+
+let row = table.insertRow(1);
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+cell1.textContent = "check";
+cell2.textContent = "check";
